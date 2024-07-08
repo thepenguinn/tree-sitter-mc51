33e8dc420763c822031a42eff4d8f07ba8286511
Daniel V Mathew <danielvmathew27@gmail.com>
2024-07-08 14:40:13 +0530

    end of testing

--- grammar.js
+++ grammar.js
@@ -7,67 +7,67 @@ module.exports = grammar({
     ],
 
     rules: {
-        // program: $ => sep(repeat1('\n'), $._item),
-        // _item: $ =>
-        //     choice(
-        //         $.meta,
-        //         $.label,
-        //         $.instruction,
-        //     ),
+        program: $ => sep(repeat1('\n'), $._item),
+        _item: $ =>
+            choice(
+                $.meta,
+                $.label,
+                $.instruction,
+            ),
 
-        // meta: $ =>
-        //     seq(
-        //         field('kind', $.meta_ident),
-        //         optional(choice(
-        //             $.ident,
-        //             seq($.int, repeat(seq(',', $.int))),
-        //             seq($.float, repeat(seq(',', $.float))),
-        //             seq($.string, repeat(seq(',', $.string))),
-        //         )),
-        //     ),
+        meta: $ =>
+            seq(
+                field('kind', $.meta_ident),
+                optional(choice(
+                    $.ident,
+                    seq($.int, repeat(seq(',', $.int))),
+                    seq($.float, repeat(seq(',', $.float))),
+                    seq($.string, repeat(seq(',', $.string))),
+                )),
+            ),
 
-        // _indent: $ => /    /,
+        // _indent: $ => /[ ]{4}/,
 
-        block_me: $ => seq(':', $.word),
+        // block_me: $ => seq($.word, '@', $.word, '@'),
 
-        // label: $ => seq($.ident, ':', optional(seq('(', $.ident, ')'))),
-        // instruction: $ => seq(field('kind', $.word), sep(',', $._expr)),
+        label: $ => seq($.ident, ':', optional(seq('(', $.ident, ')'))),
+        instruction: $ => seq(field('kind', $.word), sep(',', $._expr)),
 
-        // _expr: $ => choice($.ptr, $.ident, $.int, $.string, $.float),
-        // ptr: $ =>
-        //     choice(
-        //         seq(
-        //             optional(seq(choice('byte', 'word', 'dword', 'qword'), 'ptr')),
-        //             '[',
-        //             $.reg,
-        //             optional(seq(choice('+', '-'), choice($.int, $.ident))),
-        //             ']',
-        //         ),
-        //         seq(
-        //             optional($.int),
-        //             '(',
-        //             $.reg,
-        //             ')',
-        //         ),
-        //         seq(
-        //             '*',
-        //             'rel',
-        //             '[',
-        //             $.int,
-        //             ']',
-        //         ),
-        //     ),
+        _expr: $ => choice($.ptr, $.ident, $.int, $.string, $.float),
+        ptr: $ =>
+            choice(
+                seq(
+                    optional(seq(choice('byte', 'word', 'dword', 'qword'), 'ptr')),
+                    '[',
+                    $.reg,
+                    optional(seq(choice('+', '-'), choice($.int, $.ident))),
+                    ']',
+                ),
+                seq(
+                    optional($.int),
+                    '(',
+                    $.reg,
+                    ')',
+                ),
+                seq(
+                    '*',
+                    'rel',
+                    '[',
+                    $.int,
+                    ']',
+                ),
+            ),
 
-        // int: $ => /-?([0-9][0-9_]*|(0x|\$|#0x|#0b|#0d|#)[0-9A-Fa-f][0-9A-Fa-f_]*)/,
-        // float: $ => /-?[0-9][0-9_]*\.([0-9][0-9_]*)?/,
-        // string: $ => /"[^"]*"/,
+        int: $ => /-?([0-9][0-9_]*|(0x|\$|#0x|#0b|#0d|#)[0-9A-Fa-f][0-9A-Fa-f_]*)/,
+        float: $ => /-?[0-9][0-9_]*\.([0-9][0-9_]*)?/,
+        string: $ => /"[^"]*"/,
 
         word: $ => /[a-zA-Z0-9_]+/,
-        // _reg: $ => /%?[a-z0-9]+/,
-        // reg: $ => choice($._reg, $.word),
-        // meta_ident: $ => /\.[a-z_]+/,
-        // _ident: $ => /[a-zA-Z_0-9.]+/,
-        // ident: $ => choice($._ident, $.meta_ident, $.reg),
+        _reg: $ => /%?[a-z0-9]+/,
+        reg: $ => choice($._reg, $.word),
+        meta_ident: $ => /\.[a-z_]+/,
+        _ident: $ => /[a-zA-Z_0-9.]+/,
+        ident: $ => choice($._ident, $.meta_ident, $.reg),
 
         line_comment: $ => /(\/\/|;).*/,
         block_comment: $ =>
